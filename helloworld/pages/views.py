dbf9ebb692300bd409304bfa57699d1005a7cd52
davidbermudez <david@DESKTOP-UCQDOKF.localdomain>
2021-02-05 13:00:18 +0100

    Pruebas en helloword

--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def home_page_view(request):
     return HttpResponse("¡Hola mundo!")
+
+def about_page_view(request):
+    return HttpResponse("¡About us!")
+
+# En python, las funciones separan las palabras con el caracter subrayado, y las clases con CamelKey
+
