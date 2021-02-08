13b330ccf888d8b1a094fb4fa107b0b44389c034
davidbermudez <david@DESKTOP-UCQDOKF.localdomain>
2021-02-08 14:59:11 +0100

    aplicacion pages

--- pages/pages/views.py
+++ pages/pages/views.py
@@ -1,9 +1,9 @@
-#from django.shortcuts import render
-
 from django.views.generic import TemplateView
 
+
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
+
 
 class AboutPageView(TemplateView):
-    template_name = 'about.html'
+    template_name = "about.html"
--- pages/pages_project/settings.py
+++ pages/pages_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*-w&p%b4recu19uz2a(+hxvbg1lj&j)kv^c425bv&ew!s^-96!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'Europe/Madrid'
 
